7b860ee094e2661125f56c131649d8e24402bbc3
ShereeAnne <92857375+ShereeAnne@users.noreply.github.com>
2022-11-16 11:28:15 +0200

    update

--- ft_printf/ft_print.c
+++ ft_printf/ft_print.c
@@ -1,9 +1,20 @@
 
-
+#include "libftprintf.h"
+#include <stdarg.h>
 
 int    ft_printf(const char *, ...)
+{	
+	
+	    va_list arg; 
+    	va_start(arg, *);
+
+}
+
+
+int main ()
 {
 
-
+	const char character = "loop checking % this";
+	ft_printf("%s", character);
 
 }
---
+++ ft_printf/libftprintf.h
@@ -0,0 +1,8 @@
+#ifndef LIBFTPRINTF_H
+# define LIBFTRINTF_H
+#include "libft.h"
+
+int    ft_printf(const char *, ...);
+
+
+#endif 
