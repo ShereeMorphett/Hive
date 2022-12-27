5aa925b15ab1ab6d0e8cb5ce2251ac67661dcb1a
ShereeAnne <92857375+ShereeAnne@users.noreply.github.com>
2022-12-27 16:46:58 +0200

    update

--- Fract-Ol/practice.c
+++ Fract-Ol/practice.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <mlx.h>
+#include "keysymdef.h"
 
 typedef struct  s_program
 {
@@ -7,16 +8,21 @@ typedef struct  s_program
     void *win;
 }               t_program;
 
+int	close(int keycode, t_program *vars)
+{
+	mlx_destroy_window(vars->mlx, vars->win);
+	free(vars->win);
+	free(vars->mlx);
+	return (0);
+}
+
 int main()
 {
 	t_program	fract;
 
 	fract.mlx = mlx_init();
 	fract.win = mlx_new_window(fract.mlx, 2000, 1040, "Fract'ol");
+	mlx_hook(fract.win, 1, 1L<<0, close, &fract);
 	mlx_loop(fract.mlx);
-	
-	mlx_destroy_window(fract.mlx, fract.win);
-	free(fract.mlx);
-
-
+	return (0);
 }
