2f98a751befdf7da51706588723d2c678f1ffafb
ShereeAnne <92857375+ShereeAnne@users.noreply.github.com>
2022-12-30 12:08:29 +0200

    up

--- Fract-Ol/fractol.h
+++ Fract-Ol/fractol.h
@@ -2,9 +2,14 @@
 #include "colours.h"
 #include "mlx.h"
 #include "key_maps.h"
-# define WINDOW_WIDTH 600
-# define WINDOW_HEIGHT 400
+# define WINDOW_WIDTH 1080
+# define WINDOW_HEIGHT 900
 
+typedef struct		struct_complex_num
+{
+	double		real;
+	double		imaginary;
+}					t_complex_num;
 
 typedef struct  struct_image
 {
@@ -15,6 +20,20 @@ typedef struct  struct_image
 	int		endian;
 }               t_image;
 
+
+typedef struct		s_visual
+{
+	double		xmin;
+	double		xmax;
+	double		ymin;
+	double		ymax;
+	double		zoom;
+	double		offx;
+	double		offy;
+	long		max;
+	t_complex_num	mouse;
+}					t_visual;
+
 typedef struct  s_program
 {
     void *mlx;
@@ -23,20 +42,16 @@ typedef struct  s_program
 
 }               t_program;
 
-typedef struct		struct_complex_num
-{
-	double		real;
-	double		imaginary;
-}					t_complex_num;
+
  
+
+
 void mandelbrot_plotting(t_program *fract);
 int key_map(int keycode, t_program *vars);
 int	prog_close(int keycode, t_program *vars);
 static int	cross_close(int keycode, t_program *vars);
 void	place_pixel(t_image *img, int x, int y, int color);
 int key_map(int keycode, t_program *vars);
-
-
 
 
 //////////////////////DELETE THESE////////////////////////////////////  
--- Fract-Ol/mandelbrot.c
+++ Fract-Ol/mandelbrot.c
@@ -1,25 +1,56 @@
 #include "fractol.h"
-#define MAX_ITERATIONS 50;
+#define MAX_ITERATIONS 1000
+# define	X_SCALE_MIN  -2.0
+# define	X_SCALE_MAX  1.0
+# define    Y_SCALE_MIN  -1.5
+# define    Y_SCALE_MAX  1.5
+
 
 int mandelbrot_placeholder(t_program *fract)
 {
 	int count;
 	int offset;
 	t_image	img;
-	int x = WINDOW_WIDTH;
-	int y = WINDOW_HEIGHT;
-
-	img.image = mlx_new_image(fract->mlx, x, y);
+	img.image = mlx_new_image(fract->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
 	img.address = mlx_get_data_addr(img.image, &img.bits_per_pixel, &img.line_length, &img.endian);
-	 offset = (y * img.line_length + x * (img.bits_per_pixel / 8));
 
-	count = 600;
-	while (count != 0)
-	{
-		place_pixel(&img, x - count, y - count, BLUE);
-		count--;
+	//////////////////////////////////////////////////////////////////////////////////////////////
+
+	double x;
+	double y = 0;
+	double x_square;
+	double y_square;
+	double x_scale = (X_SCALE_MAX - X_SCALE_MIN) / WINDOW_WIDTH;
+	double y_scale = (Y_SCALE_MAX - Y_SCALE_MIN) / WINDOW_HEIGHT;
+	int iteration = 0;
+
+
+
+	while (y <= WINDOW_HEIGHT)
+	{ 
+		x = 0;
+		while (x < WINDOW_WIDTH)
+		{	
+			while (x_square + y_square <= 4 && iteration < MAX_ITERATIONS)
+			{
+				y = 2 * x * y_scale;
+				x = x_square - y_square + x_scale;
+				x_square = x * x;
+				y_square = y * y;
+				iteration++;
+			}
+			x++;
+		}
+
+		if (iteration == 100)
+			place_pixel(&img, x, y, PINK);
+		else
+			place_pixel(&img, x, y, GREEN);
+
+		y++;
 	}
-	mlx_put_image_to_window(fract->mlx, fract->win, img.image, 0, 0);
-	
+
+	mlx_put_image_to_window(fract->mlx, fract->win, img.image, 0, 0);	
 	return (0);
+
 }
