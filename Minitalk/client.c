c4a837debc84b2c845390f84c3cb5fedb5b9f5ce
Sheree Morphett <sheree@Marks-Air.lan>
2023-01-24 17:24:18 +0200

    minitalk, largely functioning

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -1,20 +1,48 @@
 #include "minitalk.h"
 
-void print_errors()
+static void	send_bits(int server_pid, char letter)
 {
-    ft_putendl_fd("ERROR MESSAGES WILL BE HERE SOME DAY", 1);
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((letter & (0x01 << bit)) != 0)
+			kill(server_pid, SIGUSR1);
+		else
+			kill(server_pid, SIGUSR2);
+		usleep(100);
+		bit++;
+	}
+}
+
+static void print_errors()
+{
+		ft_putendl_fd("----INPUT ERROR----", 1);
+		ft_putendl_fd("Correct input: ./client <PID> <MESSAGE>", 1);
+		exit (EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[])
 {
-    int pid;
+    int index;
+    int server_pid;
+    
+    index = 0;
+    server_pid = ft_atoi(argv[1]);
     if (argc != 3)
         print_errors();
-    
-    pid = ft_atoi(argv[1]);
-    ft_putendl_fd(argv[2], 1);
-    kill(pid, 0); /* kill(pid, int sig);- If sig is zero, kill() performs error checking, but does not send a signal.
-                        You can use a sig value of zero to check whether the pid argument is valid. */
+    else
+	{
+		if (!ft_strncmp(argv[2], "EXIT", 5))
+			kill(server_pid, SIGINT);
+		while (argv[2][index] != '\0')
+		{
+            send_bits(server_pid, argv[2][index]);
+			index++;
+		}
+		send_bits(server_pid, '\n');
+	}
     return(0);
     
 }
--- Minitalk/server.c
+++ Minitalk/server.c
@@ -1,17 +1,53 @@
 #include "minitalk.h"
 
+int running = 1;
+
 static void print_pid(void)
 {
-	char	*pid;
+	char	*server_pid;
 
-	pid = ft_itoa(getpid());
-	ft_putstr_fd("pid: ", 1);
-	ft_putendl_fd(pid, 1);
-	free(pid);
+	server_pid = ft_itoa(getpid());
+	ft_putstr_fd("server pid: ", 1);
+	ft_putendl_fd(server_pid, 1);
+	free(server_pid);
 }
 
-int main(void)
+void	signal_handle(int signal)
 {
+	static int	bit;
+	static int	letter;
+	
+	if (signal == SIGINT)
+	{
+		ft_putendl_fd("Exiting program", 1);	
+		running = 0;
+    }
+	if (signal == SIGUSR1)
+		letter |= (0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_putchar_fd(letter, 1);
+		bit = 0;
+		letter = 0;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+		if (argc != 1 && argv[0] != NULL)
+	{
+		ft_putendl_fd("----INPUT ERROR----", 1);
+		ft_putendl_fd("Correct input: ./server", 1);
+		return (0);
+	}
     print_pid();
-    return(0);
+	ft_putendl_fd("Waiting on client program...", 1);
+	while (running)
+	{
+		signal(SIGUSR1, signal_handle);
+		signal(SIGUSR2, signal_handle);
+		pause ();
+	}
+    exit (EXIT_SUCCESS);
 }
