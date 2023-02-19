7af8f5b353b18cb3e5d32634d95ef95fc376d517
ShereeAnne <92857375+ShereeAnne@users.noreply.github.com>
2023-02-19 15:44:48 +0200

    exam update

--- exam_prep/level_1/search_and_replace.c
+++ exam_prep/level_1/search_and_replace.c
@@ -35,39 +35,52 @@ ZaZ eT David aiME le METal.$
 $>./search_and_replace "wNcOre Un ExEmPle Pas Facilw a Ecrirw " "w" "e" | cat -e
 eNcOre Un ExEmPle Pas Facile a Ecrire $ */
 
-int ft_strlen(char *string)
+int ft_strlen(char *str)
 {
+
 	int index;
 	index = 0;
 
-	while (string[index] != '\0')
+	while (str[index])
+	{
 		index++;
-	return (index);
-
+	}
+	return(index);
 }
+
+
 int main(int argc, char *argv[])
 {
-	
 	int index = 0;
-	if (argc != 4 || ft_strlen(argv[2]) != 1 || ft_strlen(argv[3]) != 1)
+	char letter;
+
+	if (argc != 4)
+	{
+		write(1, "\n", 1);	
+		return (0);
+	}
+	if (ft_strlen(argv[2]) != 1)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+		if (ft_strlen(argv[3]) != 1)
 	{
 		write(1, "\n", 1);
 		return (0);
 	}
 
-	int len = ft_strlen(argv[1]);
-
-	while (index <= len)
+	while (argv[1][index])
 	{
-		if (argv[1][index] == *argv[2])
-		{
-			argv[1][index] = *argv[3]; 
-		}
+		if (argv[1][index] == argv[2][0])
+			letter = argv[3][0];
+		else
+			letter = argv[1][index];
+		write(1, &letter, 1);
 		index++;
 	}
-	write(1, argv[1], len);
+
 	write(1, "\n", 1);
-	
  	return (0);
 }
 
