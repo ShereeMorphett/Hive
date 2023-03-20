c3b35d9a653d3641b9c378ca92eddc4296a49c33
Sheree Morphett <sheree@Marks-Air.lan>
2023-03-20 20:19:04 +0200

    vaguely

--- push_swap/sorting.c
+++ push_swap/sorting.c
@@ -47,23 +47,6 @@ int	if_three(t_stack *data, int exit)
 	return (0);
 }
 
-int check_b_sorted(t_stack *data)
-{
-	int index;
-
-	index = 0;
-	if (data->stack_b_size > 2)
-	{
-		while (data->b[index] > data->b[index + 1])
-			index++;
-		if (index < data->size)
-			return (1);
-		else
-			return (0);
-	}
-	return (1);
-}
-
 int check_a_sorted(t_stack *data)
 {
 	int index;
@@ -79,63 +62,78 @@ int check_a_sorted(t_stack *data)
 		else
 			return (0);
 	}
-	else return (1);
+	else
+		return (1);
+}
 
+int check(t_stack *data, int max_size)
+{
+	if (data->size == max_size)
+	{
+		ft_printf("\n %i \n", check_sorted(data, 0));
+		return (check_sorted(data, 0));
+		
+	}
+	else
+		return (1);
 }
 
 void push_back(t_stack *data)
 {
-	if (data->a[0] > data->a[1])
-		swap_a(data, 1);
-	push_a(data);
-}
- void if_five(t_stack *data)
-{
-	find_range(data);
-	//ft_printf("max: %i		min: %i", data->max, data->min);
-	// ft_printf("\nSTART\n");
-	// print_stack(data);
-	// ft_printf("\n\n");
-	while (data->size > 2)
-	{
-		// ft_printf("\n\n");
-		// print_stack(data);
-		// ft_printf("\n\n");
-		if (data->a[0] == data->max)
-			rotate_a(data, 1);
-		if (check_a_sorted(data) == 0)
-			break;
-		else if (data->a[0] < data->a[1])
-			push_b(data);
-		else if (data->a[0] > data->a[1])
-		{
-			swap_a(data, 1);
-			push_b(data);
-		}
-		if (data->stack_b_size == 2)
-		{
-			if (data->b[0] < data->b[1])
-				swap_b(data, 1);
-		}
-		if (data->stack_b_size > 2)
-		{
-			if (data->a[0] > data->a[1] && data->b[0] < data->b[1])
-				swap_both(data);
-			if (data->b[0] == data->min)
-				rotate_b(data, 1);
-			if (data->b[0] < data->b[1])
-				swap_b(data, 1);
-		}
-	}
 	while (data->stack_b_size != 0)
 	{
-		push_back(data);
+		if (data->a[0] > data->a[1])
+			swap_a(data, 1);
+		push_a(data);
 		if (data->a[0] > data->a[1])
 			swap_a(data, 1);
 	}
-	if (data->a[0] != data->min  && data->a[1] == data->min)
-		swap_a(data, 1);
-	//  ft_printf("\nFINAL\n");
-	//  print_stack(data);
+}
+ void if_five(t_stack *data)
+{
+	int max_size;
+	
+	max_size = data->size;
+	find_range(data);
+	while (check(data, max_size) != 0)
+	{
+		while (data->size > 2)
+		{
+			if (check_a_sorted(data) == 0)
+				break;
+			if (data->a[data->size - 1] == data->min)
+				reverse_a(data, 1);
+			if (data->a[0] == data->max)
+				rotate_a(data, 1);
+			else if (data->a[0] < data->a[1])
+				push_b(data);
+			else if (data->a[0] > data->a[1])
+			{
+				swap_a(data, 1);
+				push_b(data);
+			}
+			if (data->stack_b_size == 2)
+			{
+				if (data->b[0] < data->b[1])
+					swap_b(data, 1);
+			}
+			if (data->stack_b_size > 2)
+			{
+				if (data->a[0] > data->a[1] && data->b[0] < data->b[1])
+					swap_both(data);
+				if (data->b[0] == data->min)
+					rotate_b(data, 1);
+				if (data->b[0] < data->b[1])
+					swap_b(data, 1);
+			}
+		}
+		while (data->size != max_size)
+		{
+			push_back(data);
+		}
+		if (data->a[0] != data->min && data->a[1] == data->min)
+			swap_a(data, 1);
+
+	}
 	check_sorted(data, 1);
 }
