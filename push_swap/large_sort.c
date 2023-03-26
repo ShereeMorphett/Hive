a93210f1669ad11c41ca52a0ba0311ac4f702710
Sheree Morphett <sheree@Marks-Air.lan>
2023-03-26 20:50:46 +0300

    push_swap 10

--- push_swap/large_sort.c
+++ push_swap/large_sort.c
@@ -10,90 +10,79 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
+/* a function that shows how many rotations are needed to be at top of stack
 
-int find_cost(t_stack *data)
+*/
+
+
+int cost_to_move_to_top_b(t_stack *data, int index_a)
 {
-	int index;
+	int index_b;
+	int cost;
 
-	index = 0;
-	if (data->b[0] > data->a[0])
-		return (0);
-	while (index < data->size - 1)
-	{
-		if (data->b[0] > data->a[index])
-			index++;
-		if (data->b[0] < data->a[index] && data->b[0] < data->a[index + 1])
+	index_b = 0;
+	while (index_b < data->stack_b_size)
+	{	
+		if (data->a[index_a] < data->b[index_b])
+			index_b++;
+		else
 			break;
 	}
-	return (index);
+	if (index_b < (data->stack_b_size / 2))
+	{
+		cost = (data->stack_b_size - 1) - index_b;
+		return (cost);
+	}
+	else
+		return (index_b + 1);
 }
 
-void	do_moves(t_stack *data)
+int cost_to_move_to_top_a(t_stack *data, int index_a)
 {
-	int cost;
-	int temp_cost;
+	if (index_a > (data->size / 2))
+		return ((data->size - 1) - index_a);
+	else
+		return (index_a);
+}
 
-	while (do_optimal(data) != 0) 
-		;
-	
-	cost = find_cost(data);
-	if (cost == 0)
-		push_a(data);
-	if (cost <= data->size / 2)
+void find_cheapest(t_stack *data)
+{
+	int index;
+	int cheapest_index;
+	int lowest_cost;
+	int cost;
+
+	if (data->b[0] < data->b[1])
+		swap_b(data, 1);
+	index = 0;
+	lowest_cost = 2147483647;
+	cheapest_index = index;
+	while (index < data->size)
 	{
-		temp_cost = cost;
-		while (cost-- != 0)
-			rotate_a(data, 1);
-		push_a(data);
-		while (temp_cost-- != 0)
-			reverse_a(data, 1);
-	}
-	else if (cost > data->size / 2)
-	{ 
-		cost = data->size - cost;	
-		temp_cost = cost;
-		while (cost >= 0)
+		cost = cost_to_move_to_top_b(data, index);
+		cost += cost_to_move_to_top_a(data, index);
+		ft_printf("\n\nnumber: %i	cost to move to correct position in b %i\n\n", data->a[index], cost);
+		if (lowest_cost > cost)
 		{
-			reverse_a(data, 1);
-			cost--;
+			lowest_cost = cost;
+			cheapest_index = index;
 		}
-		while (temp_cost-- > 0)
-			rotate_a(data, 1);
-		if (cost == 0)
-		{
-			push_a(data);
-			rotate_a(data, 1);
-		}
+		index++;
 	}
+	ft_printf("\n\n the cheapest number: %i	cost to move to correct position in b %i\n\n", data->a[cheapest_index], lowest_cost);
+	return(cheapest_index);
 }
 
 void	large_sort(t_stack *data)
 {
-	int max_size;
-
-	max_size = data->size;
+	int cheapest_index;
 	find_range(data);
-	do_optimal(data);
 	while (data->stack_b_size != 2)
 	{
-		if (data->a[0] != data->max)
-			push_b(data);
-		else
-			rotate_a(data, 1);
-	}
-	while (data->size > 3)
-	{
-		while (do_optimal(data) != 0) 
-			;
 		push_b(data);
 	}
-	if_three(data, 0);
-	//////////////////////////////////////////////////////////////////////
-	while (data->stack_b_size != 0)
-	{
-		do_moves(data);
-		//push_a(data);
-	}
+	cheapest_index = find_cheapest(data);
+	// from here i need to take that information and action it into the correct place, the create a loop to sort all but 3 from A into B
+	
 	print_stack(data);
-	check_sorted(data, 1);
 }
