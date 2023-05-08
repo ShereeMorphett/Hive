f66daeb82c412f0ef272b5bfba2c5fefe7ea6350
Sheree Morphett <sheree@Marks-Air.lan>
2023-05-08 23:28:58 +0300

    study

---
+++ exam_prep/rank03practice/get_next_line.h
@@ -0,0 +1,13 @@
+#ifndef GET_NEXT_LINE_H
+    #define GET_NEXT_LINE_H
+    #ifndef BUFFER_SIZE
+    #define BUFFER_SIZE 50
+    #endif
+
+char	  *get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+int	ft_strlen(char *s);
+
+
+#endif
